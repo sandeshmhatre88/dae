ea00ae6dd98adbfe913168e99d1996bceb843a22
sandeshmhatre88 <sandeshmhatre88@gmail.com>
2026-09-05 07:31:54 +0530

    Add console log to AuthLayout for rendering info

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,6 +1,7 @@
 import { Stack } from 'expo-router';
 
 export default function AuthLayout() {
+  console.log('[AuthLayout] rendering');
   return (
     <Stack initialRouteName="login" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="login" />
